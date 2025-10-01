95163b47eb3e20b989f783ac1f54a4792bb0afde
MANIKANDAN-17K <mass00017@gmail.com>
2025-10-01 22:39:44 +0530

    Initial commit of Angular ekart project

---
+++ src/app/Header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-header',
+   // template :'<h1>hello</h1>',
+    templateUrl:'./header.component.html',
+   // styles:['a{text-decoration: none; margin:0 10px;}','button{padding: 10px 20px;}','.ekart--header{width:100px; height: 70px;}']
+    styleUrls:['./header.component.css']
+}    
+)
+export class HeaderComponent{
+
+}
